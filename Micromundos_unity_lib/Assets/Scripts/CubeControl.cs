7517c6945a424ac487760a8bb277bb9e17db9c00
jpamato <juanpamato@gmail.com>
2018-02-09 19:17:46 -0300

    cube map

--- Micromundos_unity_lib/Assets/Scripts/CubeControl.cs
+++ Micromundos_unity_lib/Assets/Scripts/CubeControl.cs
@@ -16,7 +16,9 @@ public class CubeControl : MonoBehaviour {
 	void Update () {
 		if (ClientData.Instance.msgClient.GetBlock (id) != null) {
 			transform.eulerAngles = new Vector3 (0f, 0f, Mathf.Rad2Deg * ClientData.Instance.msgClient.GetBlock (id).angle_i);
-			transform.position = new Vector3 (ClientData.Instance.msgClient.GetBlock (id).loc_i.x*scale,ClientData.Instance.msgClient.GetBlock (id).loc_i.y*scale,0f);
+			transform.position = new Vector3 (ClientData.Instance.msgClient.GetBlock (id).loc_i.y*scale-0.5f*scale,
+				(1f-ClientData.Instance.msgClient.GetBlock (id).loc_i.y)*scale-0.5f*scale,
+				0f);
 		}
 	}
 }
